05ccce3327725fcecc7f3a19063ac26f782cc0b9
Callum <callum.taylor.reid@gmail.com>
2021-10-12 11:12:13 -0700

    Create landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,37 @@
 import React from "react";
+import LandingPage from "./Components/LandingPage";
 
-function App() {
-  const [data, setData] = React.useState(null);
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayLanding: true,
+      displayTimeLine: false
+    };
+    this.toggleDisplayTimeLine = this.toggleDisplayTimeLine.bind(this);
+    this.toggleDisplayPastLoafs = this.toggleDisplayPastLoafs.bind(this);
+  }
 
-  React.useEffect(() => {
-    fetch("/test")
-      .then((res) => res.json())
-      .then((data) => console.log(data.message));
-  }, []);
+  toggleDisplayTimeLine () {
+    console.log('display timeline');
+    //this.setState({displayTimeLine: true})
+  };
 
-  return (
-    <div className="App">
-      <p>App be loadded</p>
-      <p>{!data ? "Loading..." : data}</p>
-    </div>
-  );
+  toggleDisplayPastLoafs () {
+    console.log('old loafs');
+  };
+
+  render () {
+    return (
+      <div className='app'>
+        <LandingPage
+        toggleDisplayPastLoafs={this.toggleDisplayPastLoafs}
+        toggleDisplayTimeLine={this.toggleDisplayTimeLine}
+        />
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ client/src/Components/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const LandingPage = (props) => {
+  return (
+    <div className='landPageWrap'>
+        <h1 className='banner'>Is your leaven ready?</h1>
+        <button
+         className='startButton'
+         onClick={props.toggleDisplayTimeLine}
+         >Yes, let's start</button>
+        <button
+         className='pastLoafsButton'
+         onClick={props.toggleDisplayPastLoafs}
+         >No, show past loafs</button>
+    </div>
+  )
+}
+
+export default LandingPage;
